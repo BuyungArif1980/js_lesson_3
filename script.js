8aa9c544e56621d2b1c7596724d2daf0faf2b275
yungbow <buyungarif.budpar@gmail.com>
2020-04-20 15:32:59 +0700

    Lesson 7. Menggunakan Nilai Return

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const half = (number) => {
-  // Return nilai dari variable number dibagi 2
-  return number / 2;
+const check = (number) => {
+  // periksa apakah number adalah kelipatan dari 3 dan return-kan hasilnya
+  return number % 3 === 0;
 };
 
-// Defenisikan constant result
-const result = half(130);
-
-// Print pesan "Setengah dari 130 adalah ____"
-console.log(`Separuh dari 130 adalah ${result}`);
+// Panggil function check dalam kondisi statement if
+if (check(123)) {
+  console.log("Multiple of 3");
+} else {
+  console.log("Not a multiple of 3");
+}
