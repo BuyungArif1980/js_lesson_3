49fb6106401312dbb9806ef7eb4384a47f608b6f
yungbow <buyungarif.budpar@gmail.com>
2020-04-20 15:37:35 +0700

    Lesson 8. Scope (Cakupan)

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const check = (number) => {
-  // periksa apakah number adalah kelipatan dari 3 dan return-kan hasilnya
-  return number % 3 === 0;
+// Definisikan constant name
+const name = "Ninja Ken";
+
+const introduce = (name) => {
+  // Print "Saya ____" diconsole
+  console.log(`Saya ${name}`);
 };
 
-// Panggil function check dalam kondisi statement if
-if (check(123)) {
-  console.log("Multiple of 3");
-} else {
-  console.log("Not a multiple of 3");
-}
+// Panggil function introduce
+introduce("Guru Domba");
+
+// Print nilai dari constant name
+console.log(name);
