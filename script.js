68b3d6a4c402a2a80037d561d638534acb912644
yungbow <buyungarif.budpar@gmail.com>
2020-04-20 15:13:53 +0700

    Lesson 5. Menggunakan Beberapa Argument

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Tambahkan parameter name ke function
-const greet = (name) => {
-  // Print pesan "Halo, ____"
-  console.log(`Halo, ${name}`);
+// Tambahkan parameter number1 dan number2 kedalam function
+const add = (number1, number2) => {
+  // Print penjumlahaan number1 dan number2
+  console.log(number1 + number2);
 };
 
-// Panggil function greet dengan "Guru Domba" sebagai argument
-greet("Guru Domba");
+// Panggil function dengan 5 dan 7 sebagai argument
+add(5, 7);
